ec57611dd1184b579da6c76425cf81dc7f3bad84
Jacob Johnston <johnstonajacob@gmail.com>
2018-05-30 21:33:04 -0500

    eslint and more meta setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      test: 'hi',
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started TEST, edit <code>src/App.js</code> and save to reload.
+          {this.state.test}
         </p>
       </div>
     );
