fc050f7b640b3bcb5c382387ad16b8e7ca8c4e67
Jacob Johnston <johnstonajacob@gmail.com>
2018-05-30 21:09:32 -0500

    init react/server

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+
+// const routes = require('./routes/index');
+
+dotenv.config({
+  silent: true,
+});
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+function log(message) {
+  process.stdout.write(`${message}\n`);
+}
+
+if(process.env.BUILD === 'prod') app.use('/', express.static(`${__dirname}/../client/build`));
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use((req, res, next) => {
+  log(`${req.method} on ${req.url}`);
+  next();
+});
+// app.use('/', routes);
+
+app.listen(port, function () {
+  log(`Server started on port ${port}!`);
+});
