bbb616b12cacf6cf8e48ae002ca74d2556bdb66a
Jacob Johnston <johnstonajacob@gmail.com>
2018-05-31 15:52:53 -0500

    express router setup also i hate alex

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 
-// const routes = require('./routes/index');
+const employeeResponse = require('./routes/slack/employeeResponse');
+const employeeConfig = require('./routes/dash/employeeConfig');
+const meeting = require('./routes/dash/meeting');
 
 dotenv.config({
   silent: true,
@@ -24,7 +26,10 @@ app.use((req, res, next) => {
   log(`${req.method} on ${req.url}`);
   next();
 });
-// app.use('/', routes);
+
+app.use('/slack/employeeResponse', employeeResponse);
+app.use('/dash/employeeConfig', employeeConfig);
+app.use('/dash/meeting', meeting);
 
 app.listen(port, () => {
   log(`Server started on port ${port}!`);
