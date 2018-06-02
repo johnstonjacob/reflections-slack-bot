b08fd5d9afcbb8acb363f0909ad1b9993e2c63b4
Alex Donesky <adonesky@gmail.com>
2018-06-02 15:52:28 -0500

    added button and new env variables

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -2,9 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Login = props => (
+  // <div>
+  //   <button onClick={props.logIn} >Log In</button>
+  // </div>
   <div>
-    <button onClick={props.logIn} >Log In</button>
-  </div>
+    < a href = "https://slack.com/oauth/authorize?scope=identity.basic&client_id=371272898032.373869995077" > < img alt = "Sign in with Slack"
+    height = "40"
+    width = "172"
+    src = "https://platform.slack-edge.com/img/sign_in_with_slack.png"
+    srcset = "https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" / > < /a>
+    </div>
 );
 
 Login.propTypes = {
