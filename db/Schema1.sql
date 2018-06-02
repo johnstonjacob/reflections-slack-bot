dfea16a9cb8b3fccbc49d0ae6e7acf3380aefb3c
grungebob <chungrob@gmail.com>
2018-06-02 18:25:32 -0500

    Removed \connect plumstack

--- db/Schema1.sql
+++ db/Schema1.sql
@@ -1,11 +1,6 @@
-/*
-DROP DATABASE IF EXISTS plumstack;
-*/
 CREATE DATABASE plumstack;
 
 CREATE SCHEMA plumschema;
-
-\connect plumstack
 
 CREATE TABLE plumschema.employees (
   id serial PRIMARY KEY,
@@ -29,14 +24,3 @@ CREATE TABLE plumschema.response (
   resdate int,  
   meetid REFERENCES plumschema.meetings(id),
 );
-
-/*  
-mysql stuff, not postgres:
-Execute this file from the command line by typing:
- *    mysql -u student < schemaexample.sql
- *  to create the database and the tables.
-Example query:
-INSERT INTO meetings (ID, Notes, Message, MeetDate) VALUES (?, "This dude wilin", "Reflect on this and get back", [javascript current date variable], 7, 9);
-for current date,
-use javascript new date / date now. Easier to do on the javascript side.
- */
