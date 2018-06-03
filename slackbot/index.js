798ff3f69d9cb6ffa406cd09afdd60dc2a2a811b
Jacob Johnston <johnstonajacob@gmail.com>
2018-06-02 20:43:38 -0500

    RTM initial setup and webClient, responds to hello, gathers users and channels

--- slackbot/index.js
+++ slackbot/index.js
@@ -1,17 +1,40 @@
-const { WebClient } = require('@slack/client');
+const { WebClient, RTMClient } = require('@slack/client');
 const dotenv = require('dotenv');
-dotenv.config({
-  silent: true,
-});
-const token = process.env.SLACK_OAUTH;
-const web = new WebClient(token);
-const conversationId = 'CAZ0GKV6K';
 
-const postMessage = function(text) {
-  web.chat
-    .postMessage({ channel: conversationId, text })
-    .then(res => console.log(('Message Sent: ', res.ts)))
-    .catch(console.error);
-};
+dotenv.config({ silent: true });
+const token = process.env.BOT_OAUTH;
+const web = new WebClient(token);
+const rtm = new RTMClient(token);
+
+rtm.start();
+
+const userList = {};
+const channelList = {};
+
+web.users // get list of users and format into object to reference userID to name
+  .list()
+  .then((res) => {
+    res.members.map(item => (userList[item.id] = item.name));
+    console.log(userList);
+  });
+
+web.channels // get list of channels and format into object to reference channelID to name
+  .list()
+  .then((res) => {
+    console.log(res.channels[0].members); // get list of members of a channel
+    res.channels.map(item => (channelList[item.id] = item.name)); // format channels to their names in object format for easy reference
+    console.log(channelList);
+  });
+
+function postMessage(text, user = 'UAYRAJH8W', channel = 'CAZ0GKV6K') {
+  web.chat.postMessage({ channel, text }).catch(console.error);
+}
+
+rtm.on('slack_event', (type, event) => {
+  console.log(event);
+  if (type === 'message') {
+    if (event.text === 'hello') { rtm.sendMessage(`howdy, ${userList[event.user]}`, event.channel); }
+  }
+});
 
 module.exports.postMessage = postMessage;
