ee7c445d6db592adf78fdb3476e594de1c8fac24
Jacob Johnston <johnstonajacob@gmail.com>
2018-06-04 14:59:46 -0500

    login init

--- server/routes/slack/auth.js
+++ server/routes/slack/auth.js
@@ -1,29 +1,27 @@
 const express = require('express');
 
-
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  if (!req.query.code) { // access denied
+router.get('/', function(req, res) {
+  if (!req.query.code) {
+    // access denied
     return;
   }
   var data = {
     form: {
       client_id: process.env.LOGIN_CLIENT_ID,
       client_secret: process.env.LOGIN_CLIENT_SECRET,
-      code: req.query.code
-    }
+      code: req.query.code,
+    },
   };
-  request.post('https://slack.com/api/oauth.access', data, function (error, response, body) {
+  request.post('https://slack.com/api/oauth.access', data, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       // Get an auth token
-      let oauthToken = JSON.parse(body).access_token;
+      const oauthToken = JSON.parse(body).access_token;
       // OAuth done- redirect the user to wherever
-      res.redirect(__dirname + "/public/success.html");
+      res.send('success');
     }
-  })
+  });
 });
 
-
 module.exports = router;
-
