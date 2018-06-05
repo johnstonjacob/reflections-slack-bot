a2fce8e7c37eee2259d7fc2382bf89a6a5365757
Jacob Johnston <johnstonajacob@gmail.com>
2018-06-04 19:46:59 -0500

    fix scoping issue

--- server/routes/slack/auth.js
+++ server/routes/slack/auth.js
@@ -7,10 +7,11 @@ dotenv.config({ silent: true });
 const router = express.Router();
 
 router.get('/', function(req, res) {
+  const code = req.query.code;
   request(
     `https://slack.com/api/oauth.access?client_id=${process.env.LOGIN_CLIENT_ID}&client_secret=${
       process.env.LOGIN_CLIENT_SECRET
-    }&code=${req.query.code}&redirect_uri=http://206.189.170.211/slack/auth`
+    }&code=${code}&redirect_uri=http://206.189.170.211/slack/auth`
   ).then(res.send);
 });
 
