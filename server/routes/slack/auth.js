97f00b56761601fc4bdebf4299e089257003c44e
Jacob Johnston <johnstonajacob@gmail.com>
2018-06-04 19:35:12 -0500

    token routing

--- server/routes/slack/auth.js
+++ server/routes/slack/auth.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const request = require('request');
+const request = require('request-promise-native');
+const dotenv = require('dotenv');
+
+dotenv.config({ silent: true });
 
 const router = express.Router();
 
 router.get('/', function(req, res) {
-  console.log(req);
-  res.send(req);
+  request(
+    `https://slack.com/api/oauth.access
+    ?client_id=${process.env.LOGIN_CLIENT_ID}
+    &client_secret=${process.env.LOGIN_CLIENT_SECRET}
+    &code=${req.query.code}`
+  ).then(res.send);
 });
 
 module.exports = router;
