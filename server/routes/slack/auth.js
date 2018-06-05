de67d32fb2322828e56b7d0e064f9ca766ac98bd
Jacob Johnston <johnstonajacob@gmail.com>
2018-06-04 19:45:19 -0500

    redirect uri

--- server/routes/slack/auth.js
+++ server/routes/slack/auth.js
@@ -8,11 +8,9 @@ const router = express.Router();
 
 router.get('/', function(req, res) {
   request(
-    `https://slack.com/api/oauth.access
-    ?client_id=${process.env.LOGIN_CLIENT_ID}
-    &client_secret=${process.env.LOGIN_CLIENT_SECRET}
-    &code=${req.query.code}
-    &redirect_uri=http://206.189.170.211/slack/auth`
+    `https://slack.com/api/oauth.access?client_id=${process.env.LOGIN_CLIENT_ID}&client_secret=${
+      process.env.LOGIN_CLIENT_SECRET
+    }&code=${req.query.code}&redirect_uri=http://206.189.170.211/slack/auth`
   ).then(res.send);
 });
 
