d9179d7315fa4141bae330c02417c571a5738975
Alex Donesky <adonesky@gmail.com>
2018-06-06 22:10:55 -0500

    stuff

---
+++ db/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  host: '206.189.170.211',
+  user: 'plumstack',
+  password: 'plumstackgang',
+  database: 'plumstack',
+  ssl: true
+};
+
+IP: 206.189.170.211
+PORT: 5432
+DB: plumstack
+USER: plumstack
+PW: plumstackgang
+
+
+// -Calvin
+
+module.exports = {
+  postgres: {
+    database: process.env.DATABASE_NAME_TEST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD
+  },
+}
+
+//
