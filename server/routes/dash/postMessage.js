2c24439a66398f137f0405f91457fc88aeb6b8ed
Jacob Johnston <johnstonajacob@gmail.com>
2018-06-08 13:36:45 -0500

    add dm to selected student

--- server/routes/dash/postMessage.js
+++ server/routes/dash/postMessage.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const slackbot = require('../../../slackbot/index');
-const db = require('../../../db/database.js')
+const db = require('../../../db/database.js');
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  console.log(req.body);
-  let body = req.body;
+  const body = req.body;
   res.send('Message Sent');
 
   db.saveMeetings(body.notes, body.message, body.student, Date.now());
 
-
-  slackbot.postMessage(req.body.message);
+  slackbot.postMessage(body.message, body.student);
 });
 
 module.exports = router;
